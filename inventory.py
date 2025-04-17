03a9b5e05c493bd28540cba40dc030ef62b253b2
Soheil <greenroomblackroom@gmail.com>
2025-04-17 15:50:59 +0330

    Test

--- inventory.py
+++ inventory.py
@@ -7,30 +7,4 @@ new file for git branching
 """
 
 
-class AddItems:
-    def __init__(self):
-        pass
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6f0e87a796ccebf08cc58b4ec98e26a7ce542627
-    
-    def add_title(self):
-        pass
-<<<<<<< HEAD
->>>>>>> feature/add_items
-=======
 
-def add(a, b):
-    return a + b
-
-def power(b, p):
-    return b**p
->>>>>>> 0283044b51947b752701a4d73d7da11c6d16f383
-=======
-    
-    def hi_class(self):
-        pass
-    
->>>>>>> 6f0e87a796ccebf08cc58b4ec98e26a7ce542627

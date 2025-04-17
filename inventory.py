df69372afcb55d941ad1cebb54f8dd4e7501878f
parsa zarchini <zarchiniparsa68@gmail.com>
2025-04-17 15:52:45 +0330

    add name

--- inventory.py
+++ inventory.py
@@ -11,15 +11,3 @@ hamed
 new file for git branching
 
 """
-
-
-class AddItems:
-    def __init__(self):
-        pass
-    
-    def add_title(self):
-        pass
-    
-    def hi_class(self):
-        pass
-    

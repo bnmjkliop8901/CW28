e144d62f6577032281a2f026b07e49da38510737
Soheil <greenroomblackroom@gmail.com>
2025-04-17 16:36:59 +0330

    Testrrr

--- inventory.py
+++ inventory.py
@@ -6,7 +6,18 @@
 new file for git branching
 """
 
+books = [
+    {"title": "old man and sea", "writer": "Hemingway", "subject": "Novel", "available": True, "date": "2025-02-08", "clock": "20:37:32.631366"},
+    {"title": "pride and prejudice", "writer": "Jane Austen", "subject": "Novel", "available": True, "date": "2025-02-08", "clock": "20:37:32.631366"},
+    {"title": "Trout fishing in America", "writer": "Brautigan", "subject": "Novel", "available": True, "date": "2025-02-08", "clock": "20:37:32.631366"},
+    {"title": "life of pi", "writer": "Hemingway", "subject": "Novel", "available": True, "date": "2025-02-08", "clock": "20:37:32.631366"}
+]
 
 
-def add():
-    pass
+for book in books:
+    print(f"Title: {book['title']}")
+    print(f"Writer: {book['writer']}")
+    print(f"Subject: {book['subject']}")
+    print(f"Available: {book['available']}")
+    print(f"Date: {book['date']}")
+    print(f"Clock: {book['clock']}")

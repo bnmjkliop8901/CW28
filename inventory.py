4cf3337339d252c65fef1cf6f5667d041fa2ad50
parsa zarchini <zarchiniparsa68@gmail.com>
2025-04-17 16:45:57 +0330

    add func <add_item>

--- inventory.py
+++ inventory.py
@@ -7,8 +7,11 @@ import json
 new file for git branching
 """
 
+class Book():
+
+    def add_item(title= None, writer= None,subject= None,available= True):
+        
+        
 
 
-def add():
-    pass
 
